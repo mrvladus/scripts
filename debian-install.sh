d01a488062c0bfd364443f71253ceb352ae995a0
mrvladus <mrvladus@yandex.ru>
2022-05-06 21:08:26 +0300

    Update debian-install.sh

--- debian-install.sh
+++ debian-install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# ---------- PACKAGES ---------- #
+base='linux-image-amd64 nano bash-completion'
+de='gnome-core flatpak'
+looks='papirus-icon-theme'
+cli='git neofetch'
 # ---------- SU CHECK ---------- #
 if [ "$(whoami)" != "root" ]; then
     echo "Run script as root!"
@@ -7,35 +12,42 @@ fi
 # ---------- INSTALL DEBOOTSTRAP ---------- #
 if [ -f "/bin/pacman" ]; then
 	echo "Running on Arch Linux"
-	pacman -Sy debootstrap arch-install-scripts --noconfirm
+	pacman -Sy debootstrap arch-install-scripts --noconfirm --needed
 elif [ -f "/bin/apt" ]; then
 	echo "Running on Debian"
 	apt install debootstrap arch-install-scripts -y
 fi
 # ---------- CREDENTIALS ---------- #
-read -p "Hostname: " -ei "arch" hostname
 read -p "Username: " username
 read -p "Password: " password
 # ---------- PARTITIONS ---------- #
 umount -R /mnt
+umount /boot/efi
 read -p "Use cfdisk to create partitions? (Y/n) " show_cfdisk
 if [[ "$show_cfdisk" == "y" || "$show_cfdisk" == "" ]]; then
 	cfdisk
 fi
 clear && lsblk
 read -p "Root partition: " -ei "/dev/sda2" root_part
-mkfs.ext4 -F -F $root_part
-mount $root_part /mnt
+read -p "WARNING!!! Format root at $root_part? (y/N) " format_root
+if [[ "$format_root" == "y" ]]; then
+	mkfs.ext4 -F -F $root_part
+	mount $root_part /mnt
+else
+	echo "Exiting installation..."
+	exit
+fi
 read -p "Boot partition: " -ei "/dev/sda1" boot_part
-read -p "Format boot partition? (Y/n) " do_format
-if [[ "$do_format" == "y" || "$do_format" == "" ]]; then
+read -p "Format boot partition at $boot_part? (y/N) " format_boot
+if [[ "$format_boot" == "y" ]]; then
 	mkfs.vfat $boot_part
 	mkdir -p /mnt/boot/efi
 fi
 mount $boot_part /mnt/boot/efi
 # ---------- DEBOOTSTRAP ---------- #
+read -p "Select branch: stable, testing, unstable " -ei "testing" branch
 debootstrap testing /mnt
-echo "deb http://deb.debian.org/debian/ testing main contrib non-free" > /mnt/etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ $branch main contrib non-free" > /mnt/etc/apt/sources.list
 for dir in sys dev proc ; do
 	mount --rbind /$dir /mnt/$dir && mount --make-rslave /mnt/$dir
 done
@@ -55,8 +67,9 @@ echo "root:$password" | chpasswd
 useradd -mG sudo $username
 echo "$username:$password" | chpasswd
 # ---------- INSTALL PACKAGES ---------- #
-apt install linux-image-amd64 nano git neofetch bash-completion gnome-core flatpak papirus-icon-theme grub-efi-amd64 os-prober -y --no-install-recommends
+apt install $base $de $looks $cli -y --no-install-recommends
 # ---------- INSTALL BOOTLOADER ---------- #
+apt install grub-efi-amd64 os-prober -y
 grub-install
 update-grub
 # ---------- ENABLE SERVICES ---------- #
