b484683dde548f8ad16b8b2bc5065282d81c9e2a
mrvladus <mrvladus@yandex.ru>
2022-06-23 19:24:58 +0300

    Upd

--- arch-post-install.sh
+++ arch-post-install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-apps='gnome-{logs,boxes,calculator} simple-scan godot qbittorrent telegram-desktop firefox mpv file-roller evince eog'
-aur_apps='onlyoffice-bin visual-studio-code-bin bottles'
-if [[ $XDG_SESSION_DESKTOP == 'gnome' ]]; then
-	aur_apps+=" extension-manager-git adw-gtk3-git"
-elif [[ $XDG_SESSION_DESKTOP == 'xfce' ]]; then
-	aur_apps+=" xfce4-docklike-plugin"
-fi
+system='yay'
+games='gamemode lib32-gamemode steam steam-native-runtime'
+apps='gnome-logs gnome-boxes gnome-calculator simple-scan godot qbittorrent telegram-desktop firefox mpv file-roller evince eog'
+aur_apps='onlyoffice-bin visual-studio-code-bin bottles extension-manager-git adw-gtk3-git protonvpn-gui'
+pkgs="$system $games $apps $aur_apps"
 # ---------- ADD CHAOTIC AUR ---------- #
-sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-sudo pacman-key --lsign-key FBA220DFC880C036
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" >> /etc/pacman.conf
-sudo pacman -Syy $apps $aur_apps --noconfirm --needed
-# ---------- CONFIGURE DESKTOP ---------- #
-if [[ $XDG_SESSION_DESKTOP == 'gnome' ]]; then
-	gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3"
-	gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono Regular 10"
-	gsettings set org.gnome.desktop.interface font-name "Roboto Regular 11"
-	gsettings set org.gnome.desktop.interface document-font-name "Roboto Regular 11"
-	gsettings set org.gnome.desktop.interface icon-theme "Papirus"
-	gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
+read -p "Add Chaotic AUR? (Y/n) " add_aur
+if [[ "$add_aur" == "y" || "$add_aur" == "" ]]; then
+	pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+	pacman-key --lsign-key FBA220DFC880C036
+	pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm --needed
+	echo -e "\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist\n" >> /etc/pacman.conf
 fi
+# ---------- INSTALL APPS ---------- #
+pacman -Syy $pkgs --noconfirm --needed
+# ---------- CONFIGURE DESKTOP ---------- #
+gsettings set org.gnome.desktop.interface gtk-theme "adw-gtk3"
+gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono Regular 10"
+gsettings set org.gnome.desktop.interface font-name "Roboto Regular 11"
+gsettings set org.gnome.desktop.interface document-font-name "Roboto Regular 11"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
