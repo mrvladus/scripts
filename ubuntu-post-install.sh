50850917c2d5063bec7c034e2664390632f12959
mrvladus <64690334+mrvladus@users.noreply.github.com>
2022-10-21 10:58:23 +0300

    Update ubuntu-post-install.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 # GRUB
-sudo echo "GRUB_RECORDFAIL_TIMEOUT=0" >> /etc/default/grub
-sudo sed -i -e 's/quiet splash/quiet splash nvidia-drm.modeset=1/g' /etc/default/grub
+echo GRUB_RECORDFAIL_TIMEOUT=0 | sudo tee -a /etc/default/grub
 sudo update-grub
 
 # APT
 sudo apt update
+
 # Cleanup
-sudo apt purge --auto-remove snapd gnome-{characters,calculator,font-viewer,power-manager,startup-applications,shell-extension-ubuntu-dock,shell-extension-desktop-icons-ng,system-monitor,logs} eog evince gedit seahorse file-roller fonts-{beng,g*,k*,la*,lk*,lo*,na*,or*,p*,s*,t*,y*} -y
+sudo apt purge --auto-remove snapd gnome-{characters,font-viewer,power-manager,startup-applications} eog fonts-{beng,g*,k*,la*,lk*,lo*,na*,or*,p*,s*,t*,y*} -y
 rm -rf ./snap
-# Papirus icons
-sudo add-apt-repository ppa:papirus/papirus -y
-# Distrobox
-sudo add-apt-repository ppa:michel-slm/distrobox -y
-# Debs
-sudo apt install --no-install-recommends neofetch -y
-sudo apt install simple-scan gnome-tweaks android-sdk-platform-tools flatpak fonts-jetbrains-mono gjs git papirus-icon-theme distrobox -y
+
+# Install debs
+sudo apt install simple-scan gnome-tweaks android-sdk-platform-tools flatpak fonts-jetbrains-mono neofetch mpv qbittorrent telegram-desktop gthumb nvidia-driver-515 -y
+
 # Upgrade
 sudo apt upgrade -y
 
 # Flatpaks
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install org.mozilla.firefox com.mattjakeman.ExtensionManager -y
+flatpak install org.mozilla.firefox org.onlyoffice.desktopeditors com.visualstudio.code -y
 
 # NVIDIA DRIVER
-sudo apt install nvidia-driver-515 -y
 sudo systemctl disable nvidia-suspend.service nvidia-resume.service nvidia-hibernate.service
 sudo chmod u+x /usr/share/screen-resolution-extra/nvidia-polkit
 sudo nvidia-xconfig
