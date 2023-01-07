b896d1e2f1b50360be626698c9352c6d4e7be524
mrvladus <64690334+mrvladus@users.noreply.github.com>
2023-01-07 15:18:52 +0300

    Add files via upload

--- post-install/ubuntu.sh
+++ post-install/ubuntu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 
 # GRUB
 echo GRUB_RECORDFAIL_TIMEOUT=0 | sudo tee -a /etc/default/grub
@@ -13,7 +13,7 @@ echo -e "Package: *\nPin: release a=jammy-backports\nPin-Priority: 500" | sudo t
 sudo apt update
 
 # Cleanup
-sudo apt autopurge snapd gnome-{characters,power-manager,startup-applications} fonts-{beng,g*,k*,la*,lk*,lo*,na*,or*,p*,s*,t*,y*} -y
+sudo apt autopurge snapd gedit gnome-{characters,power-manager,startup-applications} fonts-{beng,g*,k*,la*,lk*,lo*,na*,or*,p*,s*,t*,y*} -y
 rm -rf ./snap
 sudo rm -rf /root/snap
 
@@ -27,10 +27,14 @@ wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > pa
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-sudo nala update
+
+# Firefox repo
+sudo add-apt-repository ppa:mozillateam/ppa
+echo -e "Package: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1001" | sudo tee /etc/apt/preferences.d/mozilla-firefox
 
 # Install debs
-sudo nala install git gnome-text-editor timeshift simple-scan android-sdk-platform-tools fonts-jetbrains-mono mpv qbittorrent ubuntu-restricted-extras code build-essential libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev nvidia-driver-525 wireguard -y
+sudo nala update
+sudo nala install git gnome-text-editor timeshift simple-scan android-sdk-platform-tools fonts-jetbrains-mono mpv qbittorrent ubuntu-restricted-extras code build-essential libasound2-dev mesa-common-dev libx11-dev libxrandr-dev libxi-dev xorg-dev libgl1-mesa-dev libglu1-mesa-dev nvidia-driver-525 wireguard curl flatpak resolvconf firefox -y
 
 # NodeJS
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash - && sudo apt-get install -y nodejs
@@ -49,7 +53,6 @@ flatpak install org.onlyoffice.desktopeditors org.gnome.Boxes org.telegram.deskt
 # NVIDIA DRIVER
 sudo systemctl disable nvidia-suspend.service nvidia-resume.service nvidia-hibernate.service
 sudo chmod u+x /usr/share/screen-resolution-extra/nvidia-polkit
-sudo nvidia-xconfig
 
 # SETUP
 sudo systemctl disable NetworkManager-wait-online.service
