fb6f21d0efbe46b8903ef5f394211e8db631592b
mrvladus <mrvladus@yandex.ru>
2023-01-28 13:49:17 +0300

    Update

--- post-install/fedora.sh
+++ post-install/fedora.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # ---------- CLEANUP ---------- #
-sudo dnf groupremove "Fonts" "Multimedia" "LibreOffice" "Firefox Web Browser" "Container Management" "Guest Desktop Agents" "Hardware Support" "Printing Support" -y
-sudo dnf remove gnome-{classic-session,text-editor,software,connections,photos,characters,contacts,tour,boxes,weather,clocks} abrt mediawriter eog evince cheese yelp totem baobab rhythmbox libreoffice* -y
+sudo dnf groupremove "Fonts" "Multimedia" "LibreOffice" "Firefox Web Browser" "Container Management" "Guest Desktop Agents" -y
+sudo dnf remove gnome-{classic-session,text-editor,connections,photos,characters,contacts,tour,boxes,weather,clocks,calculator,logs,terminal} abrt mediawriter eog evince yelp totem baobab rhythmbox libreoffice* -y
 sudo dnf autoremove -y
 sudo dnf clean all
 
@@ -13,35 +13,25 @@ echo fedora | sudo tee /etc/hostname
 
 # Configure dnf
 echo skip_if_unavailable=True | sudo tee -a /etc/dnf/dnf.conf
-echo fastestmirror=True | sudo tee -a /etc/dnf/dnf.conf
 echo defaultyes=True | sudo tee -a /etc/dnf/dnf.conf
 echo max_parallel_downloads=10 | sudo tee -a /etc/dnf/dnf.conf
-echo keepcache=True | sudo tee -a /etc/dnf/dnf.conf
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf upgrade --refresh -y
 
 # Install rpm's
-sudo dnf install papirus-icon-theme jetbrains-mono-fonts wget nodejs wireguard-tools -y
+sudo dnf install papirus-icon-theme jetbrains-mono-fonts wireguard-tools gnome-console gnome-tweaks -y
 
 # Flathub
 sudo flatpak remote-delete flathub
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-modify --enable flathub
-flatpak install flathub org.qbittorrent.qBittorrent org.mozilla.firefox org.telegram.desktop com.visualstudio.code com.mattjakeman.ExtensionManager org.gnome.TextEditor io.mpv.Mpv org.gnome.gThumb -y
-
-# nfetch
-wget -qO- https://raw.githubusercontent.com/mrvladus/nfetch/master/install.sh | sh
+sudo flatpak remote-modify --enable flathub
+flatpak install flathub org.qbittorrent.qBittorrent org.mozilla.firefox org.telegram.desktop com.visualstudio.code com.mattjakeman.ExtensionManager org.gnome.TextEditor io.mpv.Mpv org.gnome.Calendar org.gnome.gThumb org.gnome.Evince com.github.johnfactotum.Foliate com.rafaelmardojai.Blanket org.onlyoffice.desktopeditors org.gnome.Calculator org.kde.krita org.kde.kdenlive com.github.tchx84.Flatseal -y
 
 # ---------- CONFIGURE GNOME ---------- #
 gsettings set org.gnome.desktop.wm.preferences button-layout ":minimize,maximize,close"
 gsettings set org.gnome.desktop.interface icon-theme "Papirus"
-gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono Regular 10"
+gsettings set org.gnome.desktop.interface monospace-font-name "JetBrains Mono Regular 11"
 
-# ---------- INSTALL NVIDIA DRIVER ---------- #
-sudo dnf install akmod-nvidia -y
-# Restore Plymouth
-echo "options nvidia_drm modeset=1" | sudo tee -a /etc/modprobe.d/nvidia.conf && echo -e "add_drivers+=\" nvidia nvidia_modeset nvidia_uvm nvidia_drm \"\ninstall_items+=\" /etc/modprobe.d/nvidia.conf \"" | sudo tee -a /etc/dracut.conf.d/nvidia.conf && sudo dracut -f
-
-# ---------- BASH ALIASES ---------- #
-#echo "" >> .bash_aliases
-echo "up='sudo dnf upgrade --refresh -y && flatpak update -y'" >> ~/.bash_aliases
+# ---------- CONFIGURE WIREGUARD ---------- #
+sudo cp ./configs/warp.conf /etc/wireguard/
+sudo systemctl enable wg-quick@warp.service
+sudo systemctl daemon-reload
+sudo systemctl start wg-quick@warp
